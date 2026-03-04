2893bba65acd8c19f2ebed3b5d6e3f6535c4c80c
SSR <iamssrobinson@gmail.com>
2026-03-04 22:30:10 +0000

    Add sass watch script to package.json to run on start

--- 11-time-tracking-dashboard/views/mainView.js
+++ 11-time-tracking-dashboard/views/mainView.js
@@ -1,6 +1,9 @@
+const root = document.getElementById("root");
+
 function mainViewInit (dataObj) {
     console.log("mainViewInit");
     console.log("mainViewInit dataObj: ", dataObj);
+    root.innerHTML = "<h1>Hello World</h1>";
     
 }
 
