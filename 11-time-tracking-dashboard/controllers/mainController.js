8f9acbd52593b0b7eb1482846719686bbe19966d
SSR <iamssrobinson@gmail.com>
2026-03-04 21:50:35 +0000

    Setup for view to receive data or null + error message

--- 11-time-tracking-dashboard/controllers/mainController.js
+++ 11-time-tracking-dashboard/controllers/mainController.js
@@ -2,24 +2,28 @@ import mainViewInit from '../views/mainView.js';
 import ModelManager from '../models/mainModel.js';
 
 async function mainControllerInit () {
-    // initialize modules
+    // initialize controller
     console.log("mainControllerInit");
 
     // create instance of model
     const modelManager_1 = new ModelManager("Jeremy Robson");
     
-    // init data in model
+    // initialize model and data 
    try {
     await modelManager_1.mainModelInit();
    }
    catch (err) {
     console.log("controller", err.message);
+    // initialize view with null for failed fetch and err message arguments, to render failed setup with template data
+    mainViewInit(null, err.message);
    }
 
    // access data from model if successfully awaited
    console.log("controller: ", modelManager_1.data);
    console.log("controller: ", modelManager_1);
   
+   // initialize view and send data through from model, confirm success with 1 argument
+   mainViewInit(1, modelManager_1.data);
 }
 mainControllerInit();
 
