00a4cb7422bdaa82150bbf80ddf6711bf5d28b95
SSR <iamssrobinson@gmail.com>
2026-03-08 16:51:49 +0000

    Render timecard icons

--- 11-time-tracking-dashboard/components/TimeCard/TimeCard.js
+++ 11-time-tracking-dashboard/components/TimeCard/TimeCard.js
@@ -47,10 +47,12 @@
 function TimeCard (timeframe, stat) {
     // const timeframe = title.toLowerCase();
     console.log("TIMECARD", timeframe, stat);
+    console.log("TIMECARD ../../assets/images/icons/icon-", stat.title.toLowerCase());
+    const titleIconExt = stat.title.toLowerCase() === 'self care' ? 'self-care' : stat.title.toLowerCase(); 
 
     return `
         <article class='time-card'>
-            <img src='../../assets/images/icons/icon-${stat.title.toLowerCase()}.svg' class='time-card-icon'/>
+            <img src='../../assets/images/icons/icon-${titleIconExt}.svg' class='time-card-icon'/>
             <div class='stat-card'>
                 <header class='time-card-header'>
                     <h2 class='time-card-title'>
