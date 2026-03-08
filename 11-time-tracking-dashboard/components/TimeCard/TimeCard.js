9444e7a12c8f185d752bb0a947d933397109fd84
SSR <iamssrobinson@gmail.com>
2026-03-08 15:28:31 +0000

    Handle changing link click

--- 11-time-tracking-dashboard/components/TimeCard/TimeCard.js
+++ 11-time-tracking-dashboard/components/TimeCard/TimeCard.js
@@ -1,11 +1,12 @@
-function TimeCard (timeframe, timeObj) {
-    console.log("TimeCard obj:", timeObj);
+function TimeCard ({timeframes, title}) {
+    const timeframe = title.toLowerCase();
+
     return `
         <article class='time-card'>
             <div class='stat-card'>
                 <header class='time-card-header'>
                     <h2 class='time-card-title'>
-                    ${timeObj.title}
+                    ${title}
                     </h2>
                     <button type='button' class='ellipsis-btn'>
                         <img src='../../assets/images/icons/icon-ellipsis.svg' alt='menu' class='ellipsis-icon'/>
@@ -13,10 +14,10 @@ function TimeCard (timeframe, timeObj) {
                 </header>
                 <footer class='time-card-footer'>
                     <h3 class='time-card-current'>
-                        ${timeObj.timeframes[timeframe].current}
+                        ${timeframes['daily'].current}
                     </h3>
                     <p class='time-card-previous'>
-                    ${timeObj.timeframes[timeframe].previous}
+                    ${timeframes['daily'].previous}
                     </p>
                 </footer>
             </div>
--- 11-time-tracking-dashboard/views/mainView.js
+++ 11-time-tracking-dashboard/views/mainView.js
@@ -10,6 +10,7 @@ const cardsContainer = document.getElementById('time-cards-container');
 const errorMessageContainer = document.getElementById('error-message-container');
 
 // events
+window.addEventListener("click", handleTimeframe);
 
 
 // functions
@@ -25,6 +26,67 @@ function mainViewInit({data, error}) {
   }
   // render user and stats
     console.log("mainViewInit data: ", data);
+    
+  renderProfile(data.user);
+  renderStats(data.stats);
+
+    
+}
+
+function renderProfile (user) {
+   profileContainer.innerHTML = Profile(user);
+  
+}
+
+function renderStats (stats) {
+  console.log("stats", stats)
+    const fragment = document.createDocumentFragment();
+    console.log("frag", fragment)
+    stats.forEach(stat => {
+      console.log('stat', stat)
+      cardsContainer.innerHTML += TimeCard(stat);
+    });
+}
+
+function getCurrentTimeframe (e) {
+  const target = e.target;
+  console.log("getCurrentTimeframe target", e)
+  if (target.classList.contains('link')) {
+    console.log("Timeframe button clicked");
+    console.log("Timeframe button clicked", target);
+    const timeframe = target.id
+    console.log("timeframe button id: ", timeframe);
+    return timeframe;
+  }
+  return;
+}
+
+function removeSelectedTimeframe () {
+  console.log("removeSelectedTimeframe")
+  const links = document.querySelectorAll('.link');
+    console.log("removeSelectedTimeframe links", links)
+
+  links.forEach((item) => {
+    item.classList.remove('selected')
+    console.log("removeSelectedTimeframe", item);
+  });
+
+}
+
+function addSelectedTimeframe(timeframe) {
+  const links = document.querySelectorAll('.list');
+  links.forEach((item) => {
+    if (item.id === timeframe) {
+      item.classList.add('selected');
+    }
+  })
+}
+
+
+function handleTimeframe (e) {
+  const currentTimeframe = getCurrentTimeframe(e);
+  removeSelectedTimeframe();
+  addSelectedTimeframe(currentTimeframe);
 }
 
 export default mainViewInit;
