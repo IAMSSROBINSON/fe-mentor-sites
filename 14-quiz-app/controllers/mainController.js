f189c1e4e911648962fb39a3ba73864678b3b892
SSR <iamssrobinson@gmail.com>
2026-04-17 13:15:46 +0100

    Rewire stateManger and implement Switcher theme.

--- 14-quiz-app/controllers/mainController.js
+++ 14-quiz-app/controllers/mainController.js
@@ -1,29 +1,81 @@
 // imports
-import StateManger from "../models/mainModel.js";
+import StateManager from "../models/mainModel.js";
 import { mainViewInit } from "../views/mainView.js";
+import getData from '../database/db.js';
 
 // elements
+    const docEle = document.documentElement;
+    const switchContainer = document.getElementById('switch-container');
+    // const switchThumb = document.getElementById('switch-thumb');
     const sunIcon = document.getElementById('sun-icon');
     const moonIcon = document.getElementById('moon-icon');
-    console.log("sunIcon: ", sunIcon);
-    console.log("moonIcon: ", moonIcon);
 
 // events
+switchContainer.addEventListener('click', handleSwitch);
 
 // functions
 
 async function mainControllerInit() {
     console.log("mainControllerInit");
-    mainViewInit();
-    const stateManager = new StateManger();
-    await stateManager.mainModelInit();
-    console.log("mainControllerInit:", stateManager);
+    
+    try {
+        const data = await getData();
+        const currentTheme = matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
+        setNewTheme(currentTheme);
+        renderSwitchThumb();
+        console.log("initial theme:", currentTheme);
 
-   
+        const stateManager = new StateManager(data.quizzes, currentTheme);
+        mainViewInit();
+        console.log("mainController stateManager", stateManager);
+    }
+    catch (err) {
+        // render error message in UI with {data: [], error: "Error message here"}
+        console.error(err.message);
+    }
+
+
 }
 
+function setTheme (theme) {
+    docEle.dataset.theme = theme;
+    return;
+}
 
+function handleSwitch (e) {
+    console.log("Switch container clicked");
 
+    const theme = getCurrentTheme();
+    console.log("handleSwitch currentTheme", theme)
 
+    const themeSwitched = theme === 'dark' ? 'light' : 'dark';
+    setNewTheme(themeSwitched);
+
+    console.log("handleSwitch docEl dataset.theme switched to:", themeSwitched);
+
+    renderSwitchThumb();
+}
+
+function renderSwitchThumb () {
+    const theme = getCurrentTheme();
+    const switchThumb = document.querySelector('.switch-thumb');
+    console.log("switchThumb:", switchThumb, theme);
+    if (theme === 'light') {
+        switchThumb.classList.remove('moon');
+        console.log("switchThumb:", switchThumb, theme);
+
+    } else {
+        switchThumb.classList.add('moon');
+        console.log("switchThumb:", switchThumb, theme);
+    }
+}
+
+function getCurrentTheme () {
+    return docEle.dataset.theme;
+}
+
+function setNewTheme (theme) {
+    docEle.dataset.theme = theme;
+}
 
 export { mainControllerInit };
--- 14-quiz-app/models/mainModel.js
+++ 14-quiz-app/models/mainModel.js
@@ -1,97 +1,14 @@
-import getData from '../database/db.js';
-
-// async function mainModelInit() {
-//     console.log("mainModelInit");
-
-//     const data = await getData();
-//     console.log("data: ", await data.quizzes);
-// }
-
 class StateManager {
-
-    constructor () {
-            this.theme = "light";
-            this.data = [];
-            
-    }
-
-    async mainModelInit () {
-        console.log("mainModelInit");
-        const data = await getData();
-        console.log("data: ", await data.quizzes);
-
-        const isStorage = localStorage.getItem('quizApp');
-        console.log("isStorage:", isStorage ? true : false);
-
-        // if there is no local storage, handle setting it up, else set values
-        if (!isStorage) {
-            console.log("localStorageNotFound:");
-            // create object instance to store data: from getData() and theme: from getColorScheme()
-            const obj = {
-                data: data.quizzes,
-                theme: this.getColorScheme()
-            }
-
-            // set theme and data main properties on this class and assign to values from object instance
-            this.setData = obj.data;
-            this.setTheme = obj.theme;
-
-            // set theme and data to local storage, remember to update when theme changes via switch
-            localStorage.setItem('quizApp', JSON.stringify(obj));
-            console.log("localStorageHas", JSON.parse(localStorage.getItem("quizApp")));
-            console.log("StateManager set to:","\ndata:",this.data, "\ntheme:", this.theme);
-            return;
-        } else {
-            console.log("localStorageFound:");
-
-            // parse localstorage string into js object
-            const storageObj = JSON.parse(isStorage);
-            console.log("storageObj:", storageObj);
-
-            // set this class instance with data and theme key: value via setters
-            this.setData = storageObj.data;
-            this.setTheme = storageObj.theme;
-             
-            console.log("StateManager set to:","\ndata:",this.getData, "\ntheme:", this.getTheme);    
-        }
-
-       
-    }
-
-
-    set setTheme (theme) {
-        console.log("setTheme:", theme);
+    constructor (data = [], theme = "light") {
+        this.data = data;
         this.theme = theme;
     }
 
-    set setData (data) {
-        console.log("setData:", data);
-        this.data = data;
-    }
-
-    get getData () {
-        console.log("getData:", this.data);
-        return this.data;
-    }
-
-    get getTheme () {
-        console.log("getTheme:", this.theme);
-        return this.theme;
+    mainModelInit() {
+        console.log("mainModelInit");
     }
 
 
-    getColorScheme () {
-        // check if there a preferred color scheme set on the users system, if dark mode set then return dark else return light for default color theme
-        const theme = matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
-
-        // get the root html document element
-        const docEl = document.documentElement;
-
-        // set data-theme = theme, on documentElement so css will apply default color system styles, dark mode will be set in css variables applied with attribute selector [data-theme: "dark"] { // --darkMode variables set here }
-        docEl.dataset.theme = theme;
-        console.log("getColorScheme:", theme);
-        return theme;
-    }
 }
 
 export default StateManager;
