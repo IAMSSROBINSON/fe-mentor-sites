296f7a08e476740f13d57d2463386d3ed615d102
SSR <iamssrobinson@gmail.com>
2026-04-29 12:20:16 +0100

    Style track

--- 14-quiz-app/components/Question/Question.js
+++ 14-quiz-app/components/Question/Question.js
@@ -2,7 +2,9 @@ function Question (index, question, questionsArrLength) {
     return `
     <p class="question-number">Question ${index + 1} of ${questionsArrLength}</p>
     <h2 class="question">${question}</h2>
-    <input id="range" class="range" type="range" min="1" max=${questionsArrLength} value=${index + 1}>
+    <div class="range-container">
+        <input id="range" class="range" type="range" min="1" max=${questionsArrLength} value=${index + 1}>
+    </div>
     `;
 }
 
