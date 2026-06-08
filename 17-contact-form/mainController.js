b8cb07941dc0ccd8e65f0c0595d2db1ebb52de52
SSR <iamssrobinson@gmail.com>
2026-06-08 11:40:23 +0100

    Setup initial form events and handlers.

--- 17-contact-form/mainController.js
+++ 17-contact-form/mainController.js
@@ -1,8 +1,33 @@
 import { mainViewInit } from "./mainView.js";
 
+// functions
 function mainControllerInit () {
     console.log("mainControllerInit")
     mainViewInit();
+
+    // get elements
+    const form = document.getElementById("contact-us-form");
+    console.log("form :", form);
+    
+    // event listeners
+    form.addEventListener("blur", handleBlur, true);
+    form.addEventListener("submit", handleSubmit, true);
 }
+
+
+// handlers
+function handleBlur (e) {
+    console.log("ThunderCats Are Go!");
+    const target = e.target;
+    if (target.id === 'submit-button') return;
+
+    
+
+}
+
+function handleSubmit (e) {
+    e.preventDefault();
+}
+
 
 export { mainControllerInit };
