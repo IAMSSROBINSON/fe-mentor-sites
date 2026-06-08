652922e1b8b011052d2bcddc928639c65204fa6c
SSR <iamssrobinson@gmail.com>
2026-06-08 15:51:35 +0100

    Handle radio checked validation.

--- 17-contact-form/mainController.js
+++ 17-contact-form/mainController.js
@@ -1,4 +1,23 @@
-import { mainViewInit } from "./mainView.js";
+import { mainViewInit, renderInvalid, renderValid, removeValueAttr, renderValidRadio } from "./mainView.js";
+
+// states
+const fieldValues = {
+    "first-name": "",
+    "last-name": "",
+    email: "",
+    "query-type": "",
+    message: "",
+    consent: ""
+}
+
+const fieldRules = {
+    "first-name": [required],
+    "last-name": [required],
+    email: [required],
+    "query-type": [required],
+    message: [required],
+    consent: [required]
+}
 
 // functions
 function mainControllerInit () {
@@ -14,19 +33,86 @@ function mainControllerInit () {
     form.addEventListener("submit", handleSubmit, true);
 }
 
+function validateField (value, rules) {
+    for (const rule of rules) {
+        const error = rule(value);
+        if (error) return error;
+    }
+    return null;
+}
+
+function updateValue(field, value) {
+    fieldValues[field] = value;
+    console.log("fieldValues:", fieldValues);
+}
+
+function getCollection (name) {
+    return Array.from(document.querySelectorAll(`input[type="${name}"]`));
+}
+
+// rule functions
+function required (value) {
+    return typeof value !== "string" || value.trim() === "" ? "This field is required" : null;
+}
+
+function format (value) {
+    return !value.includes('@') || !value.includes('.') ? "Please enter a valid email address" : null;
+}
+
 
 // handlers
 function handleBlur (e) {
-    console.log("ThunderCats Are Go!");
     const target = e.target;
-    if (target.id === 'submit-button') return;
+    if (target?.id === 'submit-button' || !target) return;
+    const name = target.name;
+    const value = target.value;
+    const type = target?.type;
+        
+    console.log("target:", target);
+    console.log("name:", name);
+    console.log("value:", value);
 
-    
+    if (type === 'radio') {
+        handleRadio(name, value)
+        return;
+    }
+
+
+    const error = validateField(value, fieldRules[name]);
+    if (error) {
+        console.log("render invalid in ui");
+        renderInvalid(name, error);
+    } else {
+        console.log("render valid in ui");
+        renderValid(name);
+        updateValue(name, value);
+        console.log("fieldValues:", fieldValues);
+    }
+
+}
+
+function handleRadio (name, value) {
+    console.log("handleRadio");
+    const radios = Array.from(document.querySelectorAll(`input[name="${name}"]`));
+    const hasChecked = radios.filter((radio) => radio.checked);
+    if (hasChecked.length === 0) {
+        const error = validateField("", fieldRules[name]);
+        renderInvalid(name, error);
+        updateValue(name, "");
+
+        return;
+    }
+
+    renderValidRadio(name, value, radios);
+    updateValue(name, value);
+
+    console.log("handleRadio radios hasChecked", hasChecked);
 
 }
 
 function handleSubmit (e) {
     e.preventDefault();
+    console.log("Form submitted, preventDefault");
 }
 
 
--- 17-contact-form/mainView.js
+++ 17-contact-form/mainView.js
@@ -3,4 +3,50 @@ function mainViewInit () {
     console.log("mainViewInit")
 }
 
-export { mainViewInit };
+function renderInvalid (id, error) {
+    const element = document.getElementById(id);
+    const errorEl = document.getElementById(`${id}-error`);
+    errorEl.style.visibility = "visible";
+
+    if (id === "query-type" || id === "content") {
+        return;
+    }
+
+    element.style.outline = "1px solid red";
+    
+}
+
+function renderValid (id) {
+    const element = document.getElementById(id);
+    const errorEl = document.getElementById(`${id}-error`);
+
+    if (id === "query-type") {
+        console.log("element:", element);
+         parent.style.outline = "1px solid green";
+        errorEl.style.visibility = "hidden";
+        return;
+    }
+
+    errorEl.style.visibility = "hidden";
+    element.style.outline = "1px solid green";
+
+}
+
+function removeValueAttr (id) {
+     const element = document.getElementById(id);
+     element.value === "";
+}
+
+function renderValidRadio (name, value, radios) {
+
+    radios.forEach((radio) => radio.closest('.radio-group').style.border = "1px solid #86A2A5");
+
+    const checkedRadio = document.getElementById(value);
+
+    const parent = checkedRadio.closest('.radio-group');
+    parent.style.border = "1px solid #0C7D69";
+    const errorEl = document.getElementById(`${name}-error`);
+    errorEl.style.visibility = "hidden";
+}
+
+export { mainViewInit, renderInvalid, renderValid, removeValueAttr, renderValidRadio };
