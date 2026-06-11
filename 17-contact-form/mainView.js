c7bbec2c121ede3c2faf02e735a68ced42f4a76e
SSR <iamssrobinson@gmail.com>
2026-06-11 14:48:53 +0100

    Add aria-invalid and remove it for better a11y.

--- 17-contact-form/mainView.js
+++ 17-contact-form/mainView.js
@@ -6,23 +6,36 @@ function renderInvalid(fieldName, error) {
     const errorEl = document.getElementById(`${fieldName}-error`);
     errorEl.textContent = error;
     errorEl.style.opacity = "100%";
-    console.log("renderInvalid:", fieldName);
+    setAriaInvalidToAllNames(fieldName);
 
     if (fieldName === "query-type" || fieldName === "consent") return;
-        const el = document.querySelector(`input[name="${fieldName}"]`) || document.getElementById(fieldName);
-        el.classList?.remove("renderValid");
-        el.classList?.add("renderInvalid");
+
+    const el = document.querySelector(`input[name="${fieldName}"]`) || document.getElementById(fieldName);
+    el.classList?.remove("renderValid");
+    el.classList?.add("renderInvalid");
 }
 
 function renderValid (fieldName) {
     const errorEl = document.getElementById(`${fieldName}-error`);
     errorEl.style.opacity = "0";
+    removeAriaInvalidToAllNames(fieldName);
+
 
     if (fieldName === 'query-type' || fieldName === 'consent') return;
 
     const el = document.getElementById(fieldName);
     el.classList?.remove("renderInvalid");
     el.classList?.add("renderValid");
+}
+
+function setAriaInvalidToAllNames (fieldName) {
+    const elements = document.querySelectorAll(`[name="${fieldName}"]`);
+    elements.forEach(el => el.setAttribute('aria-invalid', 'true'));
+}
+
+function removeAriaInvalidToAllNames (fieldName) {
+    const elements = document.querySelectorAll(`[name="${fieldName}"]`);
+    elements.forEach(el => el.removeAttribute('aria-invalid'));
 }
 
 export { mainViewInit, renderInvalid, renderValid };
