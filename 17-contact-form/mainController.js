31173f7f8c78208b38de255b8e9b5545a818d439
SSR <iamssrobinson@gmail.com>
2026-06-11 14:02:12 +0100

    Format documents.

--- 17-contact-form/mainController.js
+++ 17-contact-form/mainController.js
@@ -22,7 +22,6 @@ const fieldRules = {
 
 // functions
 function mainControllerInit () {
-    console.log("mainControllerInit");
     mainViewInit();
 
     // elements
@@ -34,18 +33,13 @@ function mainControllerInit () {
 }
 
 function updateFieldValues (fieldName, value) {
-    // if field is query-type or consent make sure one of them is checked before updating value
-
     if (fieldName === 'query-type' || fieldName === 'consent') {
         const checkedValue = hasCheckedValue(fieldName);
         checkedValue ? fieldValues[fieldName] = checkedValue : fieldValues[fieldName] = "";
-        console.log("fieldValuesUpdatedAfter:", fieldValues);
         return;
     }
 
-    console.log("fieldValuesUpdatedBefore:", fieldValues);
     fieldValues[fieldName] = value;
-    console.log("fieldValuesUpdatedAfter:", fieldValues);
 }
 
 function getFieldNameValue (field) {
@@ -57,7 +51,6 @@ function getFieldNameValue (field) {
 }
 
 function hasCheckedValue (fieldName) {
-    // get all elements with inputs name
     const collection = Array.from(document.querySelectorAll(`input[name="${fieldName}"]`));
 
     const checkedEl = collection.filter((element => element.checked));
@@ -66,12 +59,9 @@ function hasCheckedValue (fieldName) {
 
 // validation functions
 function validateField (value, rules) {
-    console.log("validateField value:", value);
-    console.log("validateField rules:", rules);
     for (const rule of rules) {
         const error = rule(value);
         if (error) {
-            console.log("validateField error:", error);
             return error;
         };
     }
@@ -79,14 +69,10 @@ function validateField (value, rules) {
 
 function validateForm (fieldValues, fieldRules) {
     const errors = {
-
     }
 
     const fields = Object.keys(fieldValues);
     const values = Object.values(fieldValues);
-
-    console.log("validateForm fields:", fields);
-    console.log("validateForm values:", values);
 
     for (const field of fields) {
         const error = validateField(fieldValues[field], fieldRules[field]);
@@ -98,10 +84,8 @@ function validateForm (fieldValues, fieldRules) {
             renderValid(field);
         }
     }
-    console.log("validateForm errors:", errors);
     return errors;
 }
-
 
 function required (value) {
     return typeof value !== "string" || value.trim() === "" ? "This field is required" : null;
@@ -120,51 +104,36 @@ function consentRequired (value) {
 }
 
 
-
 // handlers
 function handleBlur (e) {
-    console.log("handleBlur clicked");
     if (e.target.id === 'submit-button') return;
 
     const target = e.target;
     const fieldName = e.target.name;
     const value = getFieldNameValue(target);
     const id = e.target.id;
-    console.log("blur target:", target);
-    console.log("blur fieldName:", fieldName);
-    console.log("blur value:", value);
-    console.log("blur id:", id);
-
+  
     // validate field value
     const error = validateField(value, fieldRules[fieldName]);
     if (error) {
-        // renderError and update field values with ""
         updateFieldValues(fieldName, value);
         renderInvalid(fieldName, error);
         return;
     } else {
-        // renderValid and update field values with value
         updateFieldValues(fieldName, value);
         renderValid(fieldName);
     }
-
-
 }
 
 function handleSubmit (e) {
     e.preventDefault();
-    console.log("handleSubmit clicked");
-    console.log("currentFieldValues:", fieldValues);
 
     const errorObj = validateForm(fieldValues, fieldRules);
     if (Object.values(errorObj).every(err => !err)) {
-        // form success, submit form
         console.log("handleSubmit form success submit form");
     } else {
-        // form unsuccessful, do not submit form
         console.log("handleSubmit form unsuccessful do not submit form");
     }
-    console.log("handleSubmit fieldValues:", fieldValues);
 }
 
 export { mainControllerInit };
--- 17-contact-form/mainView.js
+++ 17-contact-form/mainView.js
@@ -3,37 +3,26 @@ function mainViewInit () {
 }
 
 function renderInvalid(fieldName, error) {
-    console.log("renderInvalid fieldname error:", fieldName, error);
     const errorEl = document.getElementById(`${fieldName}-error`);
     errorEl.textContent = error;
-    // errorEl.style.visibility = "visible";
     errorEl.style.opacity = "100%";
     console.log("renderInvalid:", fieldName);
 
     if (fieldName === "query-type" || fieldName === "consent") return;
-
         const el = document.querySelector(`input[name="${fieldName}"]`) || document.getElementById(fieldName);
-        console.log("renderInvalid fieldName:", fieldName);
-        console.log("renderInvalid fieldName el:", el);
-
         el.classList?.remove("renderValid");
         el.classList?.add("renderInvalid");
-        console.log('el:', el);
 }
 
 function renderValid (fieldName) {
     const errorEl = document.getElementById(`${fieldName}-error`);
-    // errorEl.style.visibility = "hidden";
     errorEl.style.opacity = "0";
-    console.log("renderValid:", fieldName);
 
-      if (fieldName === 'query-type' || fieldName === 'consent') return;
+    if (fieldName === 'query-type' || fieldName === 'consent') return;
 
-        const el = document.getElementById(fieldName);
-        el.classList?.remove("renderInvalid");
-        el.classList?.add("renderValid");
-        console.log('el:', el);
-
+    const el = document.getElementById(fieldName);
+    el.classList?.remove("renderInvalid");
+    el.classList?.add("renderValid");
 }
 
 export { mainViewInit, renderInvalid, renderValid };
