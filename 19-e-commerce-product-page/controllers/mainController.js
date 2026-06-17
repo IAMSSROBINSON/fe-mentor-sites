92af07e71fab1ff92a921e188dd3a2e713ffe85e
SSR <iamssrobinson@gmail.com>
2026-06-17 13:43:23 +0100

    Create cart state with class architecture.

--- 19-e-commerce-product-page/controllers/mainController.js
+++ 19-e-commerce-product-page/controllers/mainController.js
@@ -1,11 +1,13 @@
 // imports
 import { mainViewInit, renderProduct } from "../views/mainView.js";
-import { mainModelInit } from "../models/mainModel.js";
+import { mainModelInit, User} from "../models/mainModel.js";
 import { productModelInit, getProducts } from "../models/productModel.js";
 
 // functions
 async function mainControllerInit () {
     console.log('mainControllerInit');
+    const user1 = new User('./assets/images/image-avatar.png');
+    console.log("user1:", user1);
     mainViewInit();
     mainModelInit();
     try {
--- 19-e-commerce-product-page/models/mainModel.js
+++ 19-e-commerce-product-page/models/mainModel.js
@@ -6,7 +6,36 @@
 // functions
 function mainModelInit () {
     console.log('mainModelInit');
+}
+
+class User {
     
+    cart = {
+        id: "c0001",
+        items: []
+    }
+
+    constructor (profileSrc) {
+        this.profileSrc = profileSrc;
+    }
+
+    getProfileSrc () {
+        return this.profileSrc;
+    }
+
+    setProfileSrc (path) {
+        this.profileSrc = path;
+    }
+
+    addItem(item) {
+        this.cart.items.push(item);
+        console.log("Item added:", "item:", item, "cart:", this.cart);
+    }
+
+    clearCart () {
+        this.cart.items = [];
+        console.log("Cart cleared:", this.cart);
+    }
 }
 
 
@@ -14,4 +43,4 @@ function mainModelInit () {
 
 
 // exports
-export { mainModelInit };
+export { mainModelInit, User };
