4d1b00384e128086fdfd13e90017a551b55fac32
SSR <iamssrobinson@gmail.com>
2026-07-28 11:42:06 +0100

    Focus trap the popout menu.

--- 19-e-commerce-product-page/views/menuView.js
+++ 19-e-commerce-product-page/views/menuView.js
@@ -1,6 +1,9 @@
 const body = document.querySelector('body');
 const blurWrapper = document.getElementById('blur-wrapper');
 const menu = document.querySelector("#menu-container");
+ const focusableElements = Array.from(menu.querySelectorAll("button, li a"));
+const firstFocusableElement = focusableElements[0];
+const lastFocusableElement = focusableElements[focusableElements.length - 1];
 
 function toggleMenu () {
     console.log("toggleMenu");
@@ -35,11 +38,34 @@ function focusHamburgerButton () {
 }
 
 function handleMenuKeyDown (e) {
-    if (e.key === "Escape") {
+    const key = e.key;
+    if (key === "Escape") {
         console.log("Escape key pressed");
         toggleMenu();
         focusHamburgerButton();
+        return;
     }
+
+   
+
+
+
+        focusableElements.forEach((ele) => console.log("ele:", ele));
+
+    const activeElement = document.activeElement;
+    
+    
+    if (key === 'Tab' && e.shiftKey && activeElement === firstFocusableElement)  {
+        e.preventDefault();
+        lastFocusableElement.focus();        
+    } 
+
+    if (key === 'Tab' && !e.shiftKey && activeElement === lastFocusableElement) {
+        e.preventDefault();
+        firstFocusableElement.focus();
+    }
+
+    console.log("activeElement", activeElement)
 
 }
 
