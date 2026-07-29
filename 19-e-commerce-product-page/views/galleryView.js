bcfcfaf9b7000f330e96a908903f6ac71d65a7f4
SSR <iamssrobinson@gmail.com>
2026-07-29 17:18:15 +0100

    Remove some redundant code, comments.

--- 19-e-commerce-product-page/views/galleryView.js
+++ 19-e-commerce-product-page/views/galleryView.js
@@ -1,56 +1,52 @@
 // functions
-function renderGallery (galleryElement, product) {
-    console.log("renderGallery:", galleryElement, product);
-    renderMainImage(galleryElement, product);
-    renderThumbnails(galleryElement, product);
+function renderGallery(galleryElement, product) {
+  console.log("renderGallery:", galleryElement, product);
+  renderMainImage(galleryElement, product);
+  renderThumbnails(galleryElement, product);
 }
 
-function renderMainImage (galleryElement, product, index = 0) {
-    const productImage = galleryElement.querySelector(".product-image");
-    const mainProductImageSrc = product.images[index];
-    productImage.className = `product-image product-${index + 1}`;
-    productImage.src = mainProductImageSrc;
+function renderMainImage(galleryElement, product, index = 0) {
+  const productImage = galleryElement.querySelector(".product-image");
+  const mainProductImageSrc = product.images[index];
+  productImage.className = `product-image product-${index + 1}`;
+  productImage.src = mainProductImageSrc;
 }
 
-function renderThumbnails (galleryElement, product) {
-    const thumbnailGalleryContainer = galleryElement.querySelector(".thumbnail-gallery-container");
-    thumbnailGalleryContainer.innerHTML = "";
-    
-    const thumbnailsArr = product.thumbnails;
-    console.log("renderThumbnails:", thumbnailsArr);
+function renderThumbnails(galleryElement, product) {
+  const thumbnailGalleryContainer = galleryElement.querySelector(
+    ".thumbnail-gallery-container",
+  );
+  thumbnailGalleryContainer.innerHTML = "";
 
-    thumbnailsArr.forEach((thumbnailSrc, index) => {
-        const button = document.createElement('button');
-        button.id = `product-${index+1}`
-        button.classList.add(`product-${index+1}-button`, 'thumbnail-button');
-        button.dataset.thumbnailNumber = `${index + 1}`;
-        if (index === 0) {
-            button.classList.add('selected');
-            button.setAttribute("tabindex", "0");
-           
-        } else {
-            button.setAttribute('tabindex', '-1');
-        }
-        
-        const img = document.createElement("img");
-        img.src = thumbnailSrc;
-        // img.classList.add('thumbnail', `product-${index + 1}`);
-        img.classList.add('thumbnail');
-        img.alt = `Luxury sneakers ${index === 0 ? "front" : index === 1 ? "back" : index === 2 ? "right side" : "left side"} view`;
-        button.appendChild(img);
-        thumbnailGalleryContainer.appendChild(button);
-    });   
+  const thumbnailsArr = product.thumbnails;
+  console.log("renderThumbnails:", thumbnailsArr);
+
+  thumbnailsArr.forEach((thumbnailSrc, index) => {
+    const button = document.createElement("button");
+    button.classList.add(`product-${index + 1}-button`, "thumbnail-button");
+    button.dataset.thumbnailNumber = `${index + 1}`;
+    if (index === 0) {
+      button.classList.add("selected");
+      button.setAttribute("tabindex", "0");
+    } else {
+      button.setAttribute("tabindex", "-1");
+    }
+
+    const img = document.createElement("img");
+    img.src = thumbnailSrc;
+    img.classList.add('thumbnail');
+    img.alt = `Luxury sneakers ${index === 0 ? "front" : index === 1 ? "back" : index === 2 ? "right side" : "left side"} view`;
+    button.appendChild(img);
+    thumbnailGalleryContainer.appendChild(button);
+  });
 }
 
 function renderSelectedThumbnailButton(allThumbnailsArr, selectedThumbnail) {
-    allThumbnailsArr.forEach((thumbnail) => {
-        thumbnail.classList.remove('selected');
-    });
-    selectedThumbnail.classList.add('selected');
+  allThumbnailsArr.forEach((thumbnail) => {
+    thumbnail.classList.remove("selected");
+  });
+  selectedThumbnail.classList.add("selected");
 }
-
-
-
 
 // exports
 export { renderGallery, renderSelectedThumbnailButton, renderMainImage };
